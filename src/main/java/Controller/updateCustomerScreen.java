ccfc2a787e358718303b9bb6962825941b7e04a4
tonywilliams24 <58370872+tonywilliams24@users.noreply.github.com>
2021-07-17 16:40:22 -0700

    Initial commit

---
+++ src/main/java/Controller/updateCustomerScreen.java
@@ -0,0 +1,33 @@
+package Controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class updateCustomerScreen {
+
+    @FXML
+    private AnchorPane anchorPane;
+
+    @FXML
+    private TextField addressTextField;
+
+    @FXML
+    private TextField postalCodeTextField;
+
+    @FXML
+    private TextField phoneNumberTextField;
+
+    @FXML
+    private TextField nameTextField;
+
+    @FXML
+    private TextField idTextField;
+
+    @FXML
+    private ComboBox<?> countryComboBox;
+
+    @FXML
+    private ComboBox<?> firstLevelDivisionComboBox;
+}
