236bf1ccfc4d357759554c9c1eb111377c185061
tonywilliams24 <58370872+tonywilliams24@users.noreply.github.com>
2021-07-20 00:33:25 -0700

    Working on navigation buttons

--- src/main/java/Controller/LoginScreen.java
+++ src/main/java/Controller/LoginScreen.java
@@ -1,4 +1,47 @@
 package Controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class LoginScreen {
+
+    @FXML
+    private AnchorPane anchorPane;
+
+    @FXML
+    private TextField usernameTextField;
+
+    @FXML
+    private PasswordField passwordPasswordField;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private Button clearButton;
+
+    @FXML
+    void submitHandler(ActionEvent loginSelected) throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/UserHomeScreen.fxml"));
+        loader.load();
+     /* UserHomeScreen userHomeScreenController = loader.getController(); To implement if I need to send any information from login screen to home screen
+        userHomeScreenController.sendSomething();  */
+        Stage stage = (Stage) ((Button) loginSelected.getSource()).getScene().getWindow();
+        Parent parentScene = loader.getRoot();
+        parentScene.getStylesheets().add("stylesheet.css");
+        stage.setScene(new Scene(parentScene));
+        stage.show();
+    }
+
 }
--- src/main/java/Controller/UserHomeScreen.java
+++ src/main/java/Controller/UserHomeScreen.java
@@ -1,9 +1,16 @@
 package Controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableView;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class UserHomeScreen {
 
@@ -21,5 +28,33 @@ public class UserHomeScreen {
 
     @FXML
     private Button appointmentDetailsButton;
+
+    @FXML
+    void appointmentDetailsButtonHandler(ActionEvent appointmentDetailsSelected) throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/AppointmentDetailScreen.fxml"));
+        loader.load();
+     /* UserHomeScreen userHomeScreenController = loader.getController(); To implement if I need to send any information from login screen to home screen
+        userHomeScreenController.sendSomething();  */
+        Stage stage = (Stage) ((Button) appointmentDetailsSelected.getSource()).getScene().getWindow();
+        Parent parentScene = loader.getRoot();
+        parentScene.getStylesheets().add("stylesheet.css");
+        stage.setScene(new Scene(parentScene));
+        stage.show();
+    }
+
+    @FXML
+    void customerDetailsButtonHandler(ActionEvent customerDetailsSelected) throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/CustomerDetailScreen.fxml"));
+        loader.load();
+     /* UserHomeScreen userHomeScreenController = loader.getController(); To implement if I need to send any information from login screen to home screen
+        userHomeScreenController.sendSomething();  */
+        Stage stage = (Stage) ((Button) customerDetailsSelected.getSource()).getScene().getWindow();
+        Parent parentScene = loader.getRoot();
+        parentScene.getStylesheets().add("stylesheet.css");
+        stage.setScene(new Scene(parentScene));
+        stage.show();
+    }
 
 }
